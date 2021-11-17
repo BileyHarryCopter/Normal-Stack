4741428efe853b70dcf33b7281cadefbffbd622d
BileyHarryCopter <90208149+BileyHarryCopter@users.noreply.github.com>
2021-11-17 14:34:47 +0300

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -1,109 +1,179 @@
 #include "stack.h"
-FILE * Logfile;
-FILE * Asserts;
+static FILE * Logfile;
+static FILE * Asserts;
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
                      //    Turn on the asserts    //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-int Assert (int expresion, FILE * file, const unsigned int line,
-            const char * function)
+void Assert (int expresion, FILE * file, const unsigned int line,
+            const char * function, const int numb_er, const char * name_er)
 {
     if (expresion == FALSE)
     {
+        fseek (file, 0L, SEEK_SET);
         fprintf (file, "Assertion failed\n");
         fprintf (file, "Problem on %u line\n", line);
-        fprintf (file, "In the function %s\n", function);
-        return ERROR;
+        fprintf (file, "In the function     %s\n", function);
+        fprintf (file, "Code of error:      %d\n", numb_er);
+        fprintf (file, "Name of error:      %s\n\n", name_er);
     }
-    return 0;
+}
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+                           // opening the Logfile
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+void LogOpen (Stack * stk)
+{
+    Logfile = fopen ("Logfile.txt", "w");
+    MyAss(Logfile != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__,
+          NULL_PTR_FILE, PR_NULL_PTR_FILE, stk);
+    fprintf (Logfile, "STATUS of the Stack:     good\n"
+                      "Logfile is opened successfully\n"
+                      "////////////////////////////////\n\n");
+}
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+                         //  closing the Logfile
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+void LogClose (Stack * stk)
+{
+    MyAss(Logfile != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__,
+          NULL_PTR_FILE, PR_NULL_PTR_FILE, stk);
+    printf ("STACK finished the work. Check the Logfile \n"
+            "//////////////////////////////////////////\n\n");
+    fseek (Logfile, 0L, SEEK_SET);
+    fprintf (Logfile, "STACK finished the work successfully\n"
+                      "Logfile is closed successfully\n"
+                      "////////////////////////////////////\n\n");
+    fclose (Logfile);
+
+}
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+                          // Hash calculating
+                         // Now it is very simple
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+unsigned long HashCalc (Stack * stk)
+{
+    unsigned long crc = 0;
+    unsigned long numb = sizeof(data_t)*(stk -> capacity);
+    char * Data = (char *)(stk -> data);
+
+    for (int i = 0; i < numb; i++)
+    {
+        crc += (*(Data + i)) * i;
+    }
+    crc += (unsigned long)(stk -> capacity + stk -> size);
+    return crc;
+}
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+                               // Hash check
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+int HashCheck (Stack * stk)
+{
+    if (stk -> hash == HashCalc (stk))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 void StackCtor (Stack * stk)
 {
-    Logfile = fopen ("Logfile.txt", "w");
-    //TODO: google.com --> fopen (return value)
-    // if (Logfile == NULL)
-    // perror ("Problem with file:");
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
+    LogOpen (stk);
+    MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__,
+          NULL_PTR_STK, PR_NULL_PTR_STK, stk);
     stk -> canary1 = BEZLAB;
     stk -> canary2 = AZEZBEK;
     stk -> capacity = CAP_SAMPLE;
-    stk -> data = (double *) ((char *) calloc ((stk -> capacity) * sizeof (double) + 2 * sizeof (long), sizeof (char)) + sizeof (long));
-    ////MyAss(stk -> data != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    * (stk -> data - 1) = ICECUBE;
-    * (stk -> data + stk -> capacity) = FACE;
+    stk -> data = (data_t *) ((char *)calloc ((stk -> capacity)*sizeof (data_t) + 2*sizeof (long), sizeof (char)) + sizeof (long));
+    *(long *)((char *)stk -> data - sizeof(long)) = ICECUBE;
+    *(long *)((char *)stk -> data + (stk -> capacity)*sizeof(data_t)) = FACE;
     stk -> size = 0;
-    StackCheck (stk);
+    stk -> hash = HashCalc (stk);
+    StackCheck (stk, ERRORS);
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 void StackDtor (Stack * stk)
 {
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    //finish program (call the simple assert)
-    StackCheck (stk);
-    //segfault fix fix;
+    StackCheck (stk, ERRORS);
     stk -> canary1 = 0;
     stk -> canary2 = 0;
     stk -> size = 0;
     stk -> capacity = 0;
-    fclose (Logfile);
-    free (stk -> data - 1);
+    LogClose (stk);
+    free ((char *)stk -> data - sizeof(long));
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 // Enc = encrease - увеличить
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 void StackResEnc (Stack * stk)
 {
-    // увеличивать стэк во время неполного заполнения
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    stk -> capacity += CAP_SAMPLE;
-    //??????? For what it?
-    stk -> data = (double *) ((char *) realloc ((char *) stk -> data - sizeof (long), (stk -> capacity) * sizeof (double) + 2 * sizeof (long)) + sizeof (long));
-    * (stk -> data + stk -> capacity) = FACE;
-    StackCheck (stk);
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
+    StackCheck (stk, ERRORS);
+    *(long *)((char *)(stk -> data) + (stk -> capacity)*sizeof(data_t)) = 0;
+
+    stk -> capacity = 2 * (stk -> capacity);
+
+    stk -> data = (data_t *)((char *)realloc ((char *)(stk -> data) - sizeof(long), (stk -> capacity)*sizeof(data_t) + 2*sizeof(long)) + sizeof(long));
+    assert (stk -> data != NULL);
+    *(long *)((char *)(stk -> data) + (stk -> capacity)*sizeof(data_t)) = FACE;
+    stk -> hash = HashCalc (stk);
+    StackCheck (stk, ERRORS);
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 // Red = reduce - уменьшить
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-void StackResRed (Stack * stk) // think about
+void StackResRed (Stack * stk)
 {
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    stk -> capacity -= CAP_SAMPLE;
-    stk -> data = (double *) ((char *) realloc ((char *) stk -> data - sizeof (long), (stk -> capacity) * sizeof (double) + 2 * sizeof (long)) + sizeof (long));
-    * (stk -> data + stk -> size) = FACE;
-    StackCheck (stk);
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
+    StackCheck (stk, ERRORS);
+
+    if (stk -> capacity != CAP_SAMPLE)
+    {
+        stk -> capacity = (stk -> capacity) / 2;
+    }
+
+    stk -> data = (data_t *)((char *)realloc ((char *)(stk -> data) - sizeof(long), (stk -> capacity)*sizeof(data_t) + 2*sizeof(long)) + sizeof(long));
+    *(long *)((char *)(stk -> data) + (stk -> capacity)*sizeof(data_t)) = FACE;
+    stk -> hash = HashCalc (stk);
+    StackCheck (stk, ERRORS);
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-void StackPush (Stack * stk, double value)
+void StackPush (Stack * stk, data_t value)
 {
-    StackCheck (stk);
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    if (stk -> size == stk -> capacity - 1) //when should you do it?
+    StackCheck (stk, ERRORS);
+
+    if (stk -> size == ((3 * stk -> capacity) / 4) - 1)
     {
         StackResEnc (stk);
     }
+
     * (stk -> data + stk -> size) = value;
     (stk -> size)++;
-    StackCheck (stk);
+    stk -> hash = HashCalc (stk);
+    StackCheck (stk, ERRORS);
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-double StackPop (Stack * stk)
+data_t StackPop (Stack * stk)
 {
-    //MyAss(stk != NULL, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    //MyAss(stk -> size == 0, Logfile, __LINE__, __PRETTY_FUNCTION__);
-    StackCheck (stk);
-    double value = * (stk -> data + stk -> size - 1);
-    * (stk -> data + stk -> size - 1) = 0.0;
-    if (stk -> size == stk -> capacity - CAP_SAMPLE)
+    MyAss((stk -> size) != 0, Logfile, __LINE__,
+          __PRETTY_FUNCTION__, ER_POP_SIZENULL, PR_ER_POP_SIZENULL, stk);
+    StackCheck (stk, ER_POP_SIZENULL);
+
+    data_t value = * (stk -> data + stk -> size - 1);
+    * (stk -> data + stk -> size - 1) = 0;
+    stk -> hash = HashCalc (stk);
+
+    if (stk -> size == ((3 * stk -> capacity) / 8))
     {
         StackResRed (stk);
     }
+
     (stk -> size)--;
+    stk -> hash = HashCalc (stk);
+    StackCheck (stk, ERRORS);
     return value;
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
@@ -113,60 +183,112 @@ void StackDump (Stack * stk, int status)
     unsigned int nprob = 0;
     switch (status)
     {
-        case OKEY:
+        case NO_ERRORS:
         {
+            fseek (Logfile, 0L, SEEK_SET);
             fprintf (Logfile, "STATUS of the Stack:     good\n");
+
+            fprintf (Logfile, "Specificator of Data:    "type_d"\n");
             fprintf (Logfile, "CANARY1 of the Stack:    %0X\n", (unsigned int) stk -> canary1);
             fprintf (Logfile, "SIZE of the Stack:       %zu\n", stk -> size);
             fprintf (Logfile, "CAPACITY of the Stack:   %zu\n", stk -> capacity);
             fprintf (Logfile, "CANARY1 of the Data:     %0X\n",
-                               (unsigned int) * (stk -> data - 1));
+                               (unsigned int)* (long *)((char *)(stk -> data) - sizeof (long)));
             fprintf (Logfile, "DATA:                    \n\n");
             for (size_t i = 0; i < stk -> capacity; i++)
             {
                 if (i < stk -> size)
                 {
-                    fprintf (Logfile, ">>> data[%zu] = %.1lf\n", i, * (stk -> data + i));
+                    fprintf (Logfile, ">>> data[%zu] = "fmt"\n", i, * (stk -> data + i));
                 }
                 else
                 {
-                    fprintf (Logfile, "    data[%zu] = %.1lf\n", i, * (stk -> data + i));
+                    fprintf (Logfile, "    data[%zu] = "fmt"\n", i, * (stk -> data + i));
                 }
             }
             fprintf (Logfile, "\nEND OF DATA\n");
             fprintf (Logfile, "CANARY2 of the Data:     %0X\n",
-                               (unsigned int) * (stk -> data + stk -> capacity));
+                               (unsigned int) * (long *)((char *)(stk -> data) + (stk -> capacity)*sizeof(data_t)));
             fprintf (Logfile, "CANARY2 of the Stack:    %0X\n", (unsigned int) stk -> canary2);
             fprintf (Logfile, "////////////////////////////////\n\n\n");
             break;
         }
-        case BAD:
+        case ERRORS:
         {
+            // так как по умолчанию считается, что со стеком всё в порядке
+            fseek (Logfile, 0L, SEEK_SET);
             fprintf (Logfile, "STATUS of the Stack:        bad\n");
-            if (stk -> capacity < 0)
+
+            if (stk == NULL)
+            {
+                fprintf (Logfile, "Pointer on the stack is NULL\n");
+                fprintf (Logfile, "Code of the error:      %d\n", NULL_PTR_STK);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_NULL_PTR_STK);
+                fprintf (Logfile, "Good buy\n");
+            }
+            if (stk -> data == NULL)
+            {
+                fprintf (Logfile, "Pointer of the data is NULL\n");
+                fprintf (Logfile, "Code of the error:      %d\n", NULL_PTR_DATA);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_NULL_PTR_DATA);
+                fprintf (Logfile, "Good buy\n");
+            }
+            if (stk -> capacity == 0)
             {
                 fprintf (Logfile, "Value of CAPACITY is wrong: %zu\n", stk -> capacity);
-            }
-            if (stk -> size == 0)
-            {
-                fprintf (Logfile, "Value of SIZE is wrong:     %zu\n", stk -> size);
+                fprintf (Logfile, "Code of the error:      %d\n", WRONG_SIZE);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_WRONG_SIZE);
             }
             if (stk -> canary1 != BEZLAB)
             {
-                fprintf (Logfile, "Initial value of CANARY1:   %0X\n", (unsigned int) 0xBE31AB);
+                fprintf (Logfile, "Stk CANARY1 error        \n");
+                fprintf (Logfile, "Code of the error:      %d\n", CANARY_BEAT);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_CANARY_BEAT);
+                fprintf (Logfile, "Initial value of CANARY1 of the stack:   %0X\n", (unsigned int) 0xBE31AB);
+                fprintf (Logfile, "Value of CANARY1 after using the stack:  %0X\n", (unsigned int) stk -> canary1);
                 fprintf (Logfile, "I'm very sad\nCause bird is dead\n");
                 fprintf (Logfile, "https://www.youtube.com/watch?v=krokDpyAWlM\n\n");
             }
             if (stk -> canary2 != AZEZBEK)
             {
-                fprintf (Logfile, "Initial value of CANARY2:   %0X\n", (unsigned int) 0xA3E3BEC);
+                fprintf (Logfile, "Stk CANARY2 error        \n");
+                fprintf (Logfile, "Code of the error:      %d\n", CANARY_BEAT);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_CANARY_BEAT);
+                fprintf (Logfile, "Initial value of CANARY2 of the stack:   %0X\n", (unsigned int) 0xA3E3BEC);
+                fprintf (Logfile, "Value of CANARY2 after using the stack:  %0X\n", (unsigned int) stk -> canary2);
                 fprintf (Logfile, "I'm very sad\nCause bird is dead\n");
                 fprintf (Logfile, "https://www.youtube.com/watch?v=krokDpyAWlM\n\n");
             }
-            fprintf (Logfile, "////////////////////////////////\n\n\n");
+            if (*(long *)((char *)stk -> data - sizeof(long)) != ICECUBE)
+            {
+                fprintf (Logfile, "Data CANARY1 error        \n");
+                fprintf (Logfile, "Code of the error:      %d\n", CANARY_BEAT);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_CANARY_BEAT);
+                fprintf (Logfile, "Initial value of CANARY1:   %0X\n", (unsigned int) 0x1CECBE);
+                fprintf (Logfile, "Value of CANARY1 after using the stack:  %0X\n", (unsigned int) *(long *)((char *)stk -> data - sizeof(long)));
+                fprintf (Logfile, "I'm very sad\nCause bird is dead\n");
+                fprintf (Logfile, "https://www.youtube.com/watch?v=krokDpyAWlM\n\n");
+            }
+            if (*(long *)((char *)stk -> data + (stk -> capacity)*sizeof(data_t)) != FACE)
+            {
+                fprintf (Logfile, "Data CANARY2 error        \n");
+                fprintf (Logfile, "Code of the error:      %d\n", CANARY_BEAT);
+                fprintf (Logfile, "Name of the error:      %s\n", PR_CANARY_BEAT);
+                fprintf (Logfile, "Initial value of CANARY2:   %0X\n", (unsigned int) 0xFACE);
+                fprintf (Logfile, "Value of CANARY2 after using the stack:  %0X\n", (unsigned int) *(long *)((char *)stk -> data + (stk -> capacity)*sizeof(data_t)));
+                fprintf (Logfile, "I'm very sad\nCause bird is dead\n");
+                fprintf (Logfile, "https://www.youtube.com/watch?v=krokDpyAWlM\n\n");
+            }
+            if (!(HashCheck (stk)))
+            {
+                fprintf (Logfile, "Problem with the Hash\n");
+                fprintf (Logfile, "Expected value of Hash: %lu\n", HashCalc (stk));
+                fprintf (Logfile, "Real value of Hash:     %lu\n", stk -> hash);
+            }
+            fprintf (Logfile, "//////////////////////////////// \n\n\n");
             break;
         }
-        default:  // придумать вариант для просмотра ошибки
+        default:
         {
             fprintf (Logfile, "     ___UNKNOWN PROBLEM___     \n");
             fprintf (Logfile, "STATUS of the Stack:    unknown\n");
@@ -183,27 +305,18 @@ void StackDump (Stack * stk, int status)
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-void StackOk (Stack * stk)
+void StackCheck (Stack * stk, int error)
 {
-    if ( (stk != NULL) || (stk -> data != NULL) || (stk -> size == 0) ||
-         (stk -> canary1) != BEZLAB || (stk -> canary2) != AZEZBEK ||
-         (* (stk -> data - 1) != ICECUBE) || (* (stk -> data + stk -> capacity) != FACE))
+    if ( (stk == NULL) || ((stk -> data) == NULL) ||
+         ((stk -> canary1) != BEZLAB) || ((stk -> canary2) != AZEZBEK) ||
+         (*(long *)((char *)stk -> data - sizeof(long)) != ICECUBE) ||
+         (*(long *)((char *)stk -> data + (stk -> capacity)*sizeof(data_t)) != FACE) ||
+         !(HashCheck (stk)))
     {
-        StackDump (stk, BAD);
+        StackDump (stk, ERRORS);
     }
-    else
+    else if (error == NO_ERRORS)
     {
-        StackDump (stk, OKEY);
-    }
-}
-//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-void StackCheck (Stack * stk)
-{
-    if ( (stk != NULL) || (stk -> data != NULL) || (stk -> size == 0) ||
-         (stk -> canary1) != BEZLAB || (stk -> canary2) != AZEZBEK ||
-         (* (stk -> data - 1) != ICECUBE) || (* (stk -> data + stk -> capacity) != FACE))
-    {
-        StackOk (stk);
+        StackDump (stk, NO_ERRORS);
     }
 }
--- stack.h
+++ stack.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,10 +6,7 @@
 enum
 {
     FALSE = 0,
-    TRUE,
-    ERROR,
-    OKEY,
-    BAD
+    TRUE
 };
 
 #define BEZLAB   0xBE31AB
@@ -18,43 +14,118 @@ enum
 #define FACE     0xFACE
 #define AZEZBEK  0xA3E3BEC
 // define для assert
-#define MyAss(expresion, file, line, func)                    \
-{                                                             \
-    if (!(expresion))                                         \
-    {                                                         \
-        Assert (FALSE, file, __LINE__, __PRETTY_FUNCTION__);  \
-        return ERROR;                                         \
-    }                                                         \
-}                                                             \
+#define MyAss(expresion, file, line, func, numb_er, name_er, stk)               \
+{                                                                               \
+    if (file == NULL)                                                           \
+    {                                                                           \
+        printf ("Assertion failed\n");                                          \
+        printf ("Problem with opening the Logfile on %u line\n", line);         \
+        printf ("In the function     %s\n", func);                              \
+        printf ("Code of error:      %d\n", NULL_PTR_FILE);                     \
+        printf ("Name of error:      %s\n\n", PR_NULL_PTR_FILE);                \
+        exit (EXIT_SUCCESS);                                                    \
+    }                                                                           \
+    else if (!(expresion))                                                      \
+    {                                                                           \
+        Assert (FALSE, file, __LINE__, __PRETTY_FUNCTION__, numb_er, name_er);  \
+        StackDtor (stk);                                                        \
+        exit (EXIT_SUCCESS);                                                    \
+    }                                                                           \
+}                                                                               \
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+                           // definitions of ERRORS
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+#define NULL_PTR_STK      10
+#define NULL_PTR_DATA     11
+#define ER_POP_SIZENULL   12
+#define NULL_PTR_FILE     13
+#define ERRORS            14
+#define NO_ERRORS         15
+#define WRONG_SIZE        16
+#define CANARY_BEAT       17
+#define HASH_ER           18
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+#define PR_NULL_PTR_STK        "NULL_PTR_STK"
+#define PR_NULL_PTR_DATA       "NULL_PTR_DATA"
+#define PR_ER_POP_SIZENULL     "ER_POP_SIZENULL"
+#define PR_NULL_PTR_FILE       "NULL_PTR_FILE"
+#define PR_ERRORS              "ERRORS"
+#define PR_NO_ERRORS           "NO_ERRORS"
+#define PR_WRONG_SIZE          "WRONG_SIZE"
+#define PR_CANARY_BEAT         "CANARY_BEAT"
+#define PR_HASH_ER             "HASH_ER"
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+              // the block with conditional compilation DATA
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+#define CHAR    0
+#define SHORT   1
+#define INT     2
+#define LONG    3
+#define FLOAT   4
+#define DOUBLE  5
 
-// оставить с const
+#define Stack_Type DOUBLE
+
+#if Stack_Type == CHAR
+    #define data_t char
+    #define fmt "%c"
+    #define type_d "char"
+#endif
+
+#if Stack_Type == SHORT
+    #define data_t short
+    #define fmt "%hi"
+    #define tipe_d "short"
+#endif
+
+#if Stack_Type == INT
+    #define data_t int
+    #define fmt "%d"
+    #define type_d "int"
+#endif
+
+#if Stack_Type == LONG
+    #define data_t long
+    #define fmt "%li"
+    #define type_d "long"
+#endif
+
+#if Stack_Type == FLOAT
+    #define data_t float
+    #define fmt "%lf"
+    #define type_d "float"
+#endif
+
+#if Stack_Type == DOUBLE
+    #define data_t double
+    #define fmt "%lf"
+    #define type_d "double"
+#endif
+
+
 static size_t CAP_SAMPLE = sizeof (double);
 struct Stack;
 typedef struct Stack Stack;
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
                          // definitions of functions
+        // it doesn't need to write functions which don't need the user
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-int Assert (int expresion, FILE * file,
-            const unsigned int line,
-            const char * function);
 void StackDump (Stack * stk, int status);
-void StackOk (Stack * stk);
-void StackCheck (Stack * stk);
+void StackCheck (Stack * stk, int error);
 void StackCtor (Stack * stk);
 void StackDtor (Stack * stk);
-void StackResEnc (Stack * stk);
-void StackResRed (Stack * stk);
-void StackPush (Stack * stk, double value);
-double StackPop (Stack * stk);
+void StackPush (Stack * stk, data_t value);
+data_t StackPop (Stack * stk);
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
                          // definition of the struct
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 struct Stack {
     long canary1;
-    double * data;
+    data_t * data;
     size_t size;
     size_t capacity;
-    // hash
+    unsigned long hash;
     long canary2;
 };
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
