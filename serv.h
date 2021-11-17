c46fcc398fa8abb9d7b5bbbe18ec6830eeafdd33
BileyHarryCopter <90208149+BileyHarryCopter@users.noreply.github.com>
2021-11-17 14:33:22 +0300

    Delete serv.h

--- serv.h
+++
@@ -1,8 +0,0 @@
-#define $meow printf ("Poltorashka do meow on %d line in %s\n", __LINE__, __PRETTY_FUNCTION__)
-void meowsert (int expresion)
-{
-    if (expresion == 0)
-    {
-        $meow;
-    }
-}
